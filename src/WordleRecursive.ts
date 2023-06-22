5fd58d5165ea79f2c378c6c571d4bbad112b7d3a
jackryanservia <90076280+jackryanservia@users.noreply.github.com>
2023-06-21 18:23:54 -0600

    Update WorldeRecursive for SnarkyJS 0.11.0

--- src/WordleRecursive.ts
+++ src/WordleRecursive.ts
@@ -2,7 +2,6 @@ import {
   SelfProof,
   Field,
   Experimental,
-  verify,
   Struct,
   UInt32,
   Poseidon,
@@ -40,88 +39,73 @@ export class WordleState extends Struct({
 }) {}
 
 export const WordleRecursive = Experimental.ZkProgram({
-  publicInput: WordleState,
+  publicOutput: WordleState,
 
   methods: {
     init: {
       privateInputs: [],
-      method(publicInput: WordleState) {
-        // Set the solution to the word 'hello'
-        publicInput.solutionHash.assertEquals(Word.fromString('hello').hash());
-        // Set the turn number to zero
-        publicInput.turnNumber.assertEquals(new UInt32(Field(0)));
-        // Set the inital guess to 'aaaaa'
-        // It won't be possible to generate a hint until the guesser updates this
-        for (let i = 0; i < 5; i++) {
-          publicInput.lastGuess.encodedWord[i].assertEquals(Field(1));
-        }
+      method() {
+        // no checks here, just producing the initial state
+        return new WordleState({
+          // Set the solution to the word 'hello'
+          solutionHash: Word.fromString('hello').hash(),
+          // Set the turn number to zero
+          turnNumber: new UInt32(Field(0)),
+          // Set the inital guess to 'aaaaa'
+          // It won't be possible to generate a hint until the guesser updates this
+          lastGuess: Word.fromString('aaaaa'),
+        });
       },
     },
 
     publishGuess: {
       privateInputs: [Word, SelfProof],
 
-      method(
-        publicInput: WordleState,
-        guess: Word,
-        previouseProof: SelfProof<WordleState>
-      ) {
-        previouseProof.verify();
-
-        const turnNumber = previouseProof.publicInput.turnNumber;
+      method(guess: Word, previousProof: SelfProof<undefined, WordleState>) {
+        previousProof.verify();
 
         // Check that it is the guessers turn
-        turnNumber.mod(2).assertEquals(new UInt32(Field(0)));
-        // Increment the turn number
-        publicInput.turnNumber.assertEquals(
-          turnNumber.add(new UInt32(Field(1)))
-        );
+        previousProof.publicOutput.turnNumber
+          .mod(2)
+          .assertEquals(new UInt32(Field(0)));
 
-        // Check that all values are between 1 and 26 (valid letter configuration)
+        // Check that all guess values are between 1 and 26 (valid letter configuration)
         for (let i = 0; i < 5; i++) {
-          guess.encodedWord[i].assertGte(Field(1));
-          guess.encodedWord[i].assertLte(Field(26));
+          guess.encodedWord[i].assertGreaterThanOrEqual(Field(1));
+          guess.encodedWord[i].assertLessThanOrEqual(Field(26));
         }
 
-        // Set lastGuess to new guess
-        for (let i = 0; i < 5; i++) {
-          publicInput.lastGuess.encodedWord[i].assertEquals(
-            guess.encodedWord[i]
-          );
-        }
+        return new WordleState({
+          // Pass solutionHash through unchanged
+          solutionHash: previousProof.publicOutput.solutionHash,
+          // Increment turnNumber
+          turnNumber: previousProof.publicOutput.turnNumber.add(1),
+          // Update guess
+          lastGuess: guess,
+        });
       },
     },
 
     publishHint: {
       privateInputs: [Word, SelfProof],
 
-      method(
-        publicInput: WordleState,
-        solution: Word,
-        previouseProof: SelfProof<WordleState>
-      ) {
-        previouseProof.verify();
-
-        const turnNumber = previouseProof.publicInput.turnNumber;
+      method(solution: Word, previousProof: SelfProof<undefined, WordleState>) {
+        previousProof.verify();
 
         // Add one and mod to check if it is hint generators turn
-        turnNumber
+        previousProof.publicOutput.turnNumber
           .add(1)
           .mod(2)
           .assertEquals(new UInt32(Field(0)));
-        // Increment the turn number
-        publicInput.turnNumber.assertEquals(
-          turnNumber.add(new UInt32(Field(1)))
-        );
 
         // Grab solution hash from the Mina network
-        const solutionHash = previouseProof.publicInput.solutionHash;
+        const solutionHash = previousProof.publicOutput.solutionHash;
 
         // Check that the solution we provide matches the one on-chain
         solutionHash.assertEquals(solution.hash());
 
         // Grab lastGuess from the Mina network
-        const lastGuess = previouseProof.publicInput.lastGuess;
+        const lastGuess = previousProof.publicOutput.lastGuess;
 
         // Check green letters
         // Iterate thorough every index
@@ -165,93 +149,15 @@ export const WordleRecursive = Experimental.ZkProgram({
           }
         }
 
-        // Set lastGuess to our new lastGuess which includes hints
-        for (let i = 0; i < 5; i++) {
-          publicInput.lastGuess.encodedWord[i].assertEquals(
-            lastGuess.encodedWord[i]
-          );
-        }
+        return new WordleState({
+          // Pass solutionHash through unchanged
+          solutionHash: previousProof.publicOutput.solutionHash,
+          // Increment turnNumber
+          turnNumber: previousProof.publicOutput.turnNumber.add(1),
+          // Update guess to reflect added hints
+          lastGuess: lastGuess,
+        });
       },
     },
   },
 });
-
-export const WordleHelper = {
-  init() {
-    // no checks here, just producing the initial state
-    return new WordleState({
-      solutionHash: Word.fromString('hello').hash(),
-      turnNumber: new UInt32(Field(0)),
-      lastGuess: Word.fromString('aaaaa'),
-    });
-  },
-
-  publishGuess(
-    guess: Word, // as before
-    previousProof: SelfProof<WordleState> // RECURSION!!!
-  ) {
-    return new WordleState({
-      ...previousProof.publicInput,
-      // Increment the turn number
-      turnNumber: previousProof.publicInput.turnNumber.add(UInt32.one),
-      // Set lastGuess to new guess
-      lastGuess: guess,
-    });
-  },
-
-  publishHint(solution: Word, previousProof: SelfProof<WordleState>) {
-    const solutionHash = previousProof.publicInput.solutionHash;
-
-    const lastGuess = previousProof.publicInput.lastGuess;
-
-    // Check green letters
-    // Iterate thorough every index
-    for (let i = 0; i < 5; i++) {
-      // Check if the letters at this index match
-      let isCorrectPosition = lastGuess.encodedWord[i].equals(
-        solution.encodedWord[i]
-      );
-
-      // If the letters match then add 200 to the letter in lastGuess
-      lastGuess.encodedWord[i] = Circuit.if(
-        isCorrectPosition,
-        lastGuess.encodedWord[i].add(Field(200)),
-        lastGuess.encodedWord[i]
-      );
-    }
-
-    // Add yellow letter hints
-    // Iterate through every possible combination of indexes
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        // Check if the letters at these indexes match
-        let doseLetterMatch = lastGuess.encodedWord[i].equals(
-          solution.encodedWord[j]
-        );
-
-        // If the letters match then add 100 to the letter in lastGuess
-        lastGuess.encodedWord[i] = Circuit.if(
-          doseLetterMatch,
-          lastGuess.encodedWord[i].add(Field(100)),
-          lastGuess.encodedWord[i]
-        );
-
-        // If the letters match then set the value in solution to zero
-        // This way we only count correct letters in the wrong position once
-        solution.encodedWord[j] = Circuit.if(
-          doseLetterMatch,
-          Field(0),
-          solution.encodedWord[i]
-        );
-      }
-
-      return new WordleState({
-        ...previousProof.publicInput,
-        turnNumber: previousProof.publicInput.turnNumber.add(
-          new UInt32(Field(1))
-        ),
-        lastGuess,
-      });
-    }
-  },
-};
--- src/WordleRecursiveRun.ts
+++ src/WordleRecursiveRun.ts
@@ -1,14 +1,7 @@
-import { isReady, UInt32, Field } from 'snarkyjs';
+import { UInt32, Field } from 'snarkyjs';
 
-import {
-  Word,
-  WordleRecursive,
-  WordleHelper,
-  WordleState,
-} from './WordleRecursive.js';
+import { Word, WordleRecursive } from './WordleRecursive.js';
 import { tic, toc } from './tictoc.js';
-
-await isReady;
 
 tic('compiling');
 await WordleRecursive.compile();
@@ -18,41 +11,32 @@ let solution = Word.fromString('hello');
 
 // initialize (== create the first proof)
 tic('prove (init)');
-let initialState = WordleHelper.init();
-let initialProof = await WordleRecursive.init(initialState); // <-- no class instantiation, just calling a function to create proof
+let initialProof = await WordleRecursive.init(); // <-- no class instantiation, just calling a function to create proof
 toc();
 
 console.log('Proof state initialized!');
+console.log(
+  'Initial guess: ' + initialProof.publicOutput.lastGuess.encodedWord.toString()
+);
 
 // to make a guess, a user would fetch the initial proof from a server, and then run this:
 
 tic('prove (guess)');
 let guess = Word.fromString('exile');
-let userState = WordleHelper.publishGuess(guess, initialProof);
-let userProof = await WordleRecursive.publishGuess(
-  userState,
-  guess,
-  initialProof
-);
+let userProof = await WordleRecursive.publishGuess(guess, initialProof);
 toc();
 
-console.log('Guess Valid!');
+console.log(
+  'Valid guess: ' + userProof.publicOutput.lastGuess.encodedWord.toString()
+);
 
 // user would now post the userProof to the server, and wait for it to publish a hint in form of another proof
 
 tic('prove (hint)');
-let serverState =
-  WordleHelper.publishHint(solution, userProof) ??
-  new WordleState({
-    solutionHash: Word.fromString('hello').hash(),
-    turnNumber: new UInt32(Field(2)),
-    lastGuess: Word.fromString('exile'),
-  });
-let serverProof = await WordleRecursive.publishHint(
-  serverState,
-  solution,
-  userProof
-);
+let serverProof = await WordleRecursive.publishHint(solution, userProof);
 toc();
 
-console.log('Guess with hints: ' + serverProof.publicInput.lastGuess);
+console.log(
+  'Guess with hints: ' +
+    serverProof.publicOutput.lastGuess.encodedWord.toString()
+);
